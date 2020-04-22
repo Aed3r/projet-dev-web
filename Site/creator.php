7d31fe278968dc23ceb6e58ef844fc4575cf7ec6
Aed3r <xxgoffi22xx@gmail.com>
2020-04-22 14:55:46 +0200

    Reparations + compartimentalisation

--- Site/creator.php
+++ Site/creator.php
@@ -25,46 +25,8 @@
     <head>
         <meta charset="UTF-8">
         <title>Créateur intéractif</title>
-        <style>
-            form.inline {
-                display:inline-flex;
-                margin:5px;
-                padding:0px;
-                justify-content: center;
-                align-items: center;
-            }
-            form.inline input  {
-                padding: 8px 15px;
-                background-color: green;
-                border: 1px solid #ddd;
-                color: white;
-                margin: 0px 10px;
-            }
-            form.inline input:hover  {
-                background-color: darkgreen;
-            }
-            #div1, #div2 {
-                float: left;
-                width: 50px;
-                height: 50px;
-                margin: 10px;
-                padding: 10px;
-                border: 1px solid black;
-            }
-        </style>
-        <script type="text/javascript">
-            function allowDrop(ev){
-                ev.preventDefault();
-            }
-            function drag(ev){
-                ev.dataTransfer.setData("text", ev.target.id);
-            }
-            function drop(ev){
-                ev.preventDefault();
-                var data = ev.dataTransfer.getData("text");
-                ev.target.appendChild(document.getElementById(data));
-            }
-        </script>
+        <link rel="stylesheet" type="text/css" href="data/creatorStyle.css">
+        <script src="data/creatorScript.js" type="text/javascript"></script>
     </head>
     <body>
     <?php
@@ -83,22 +45,17 @@
                 # L'utilisateur n'a jamais mis en ligne des images
                 mkdir($dirPath);
             }
-            echo " 
-<form class='inline' action='creator.php' method='post' enctype='multipart/form-data'>
-    <label for='image'>
-        <img src='data/img/plus.svg' draggable='true' ondragstart='drag(event)' id='drag1' alt='img' height='50'> <br>
-        <input type='file' name='image' id='image'/>
-    </label>
-    <input type='submit' value='Upload' name='submit'>
-</form>
-<br>";
         }
     ?>
-    <br>
+    <form class='inline' action='creator.php' method='post' enctype='multipart/form-data'>
+        <label for='image'>
+            <input type='file' name='image' id='image' style='display:none;'/>
+            <img src='data/img/plus.svg' alt='img' height='50'>
+        </label>
+        <input type='submit' value='Upload' name='submit'>
+    </form> <br> <br>
 
     <div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-
-    <div id="div2" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
 
     <br>
     <a href='logout.php'>Se déconnecter</a><br>
