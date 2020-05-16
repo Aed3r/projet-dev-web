031c80b09f1b17ab1aea80341a26a1c9624e7a81
Woople74 <Darkmar74@outlook.com>
2020-05-16 12:40:30 +0200

    Début de la mise en forme du site

--- Site/index.php
+++ Site/index.php
@@ -8,15 +8,23 @@ session_start();
     </style>
 </head>
 <body>
-<?php
-if(!isset($_SESSION["pseudo"])) {
-    echo "<h3>Vous n'êtes pas connecté(e)</h3><br>";
-    echo "<a href='inscription.php'>S'inscrire</a><br>";
-    echo "<a href='connexion.php'>Se connecter</a>";
-} else {
-    header('Location:creator.php');
-}
-?>
-
+	<nav>
+		<a href='index.php'><img src='data/img/iconne_site.png' alt='iconne_site' height='100'></a><br>
+		<?php
+		if(!isset($_SESSION["pseudo"])) {
+		    echo "<h3>Vous n'êtes pas connecté(e)</h3><br>";
+		    echo "<a href='inscription.php'>S'inscrire</a><br>";
+		    echo "<a href='connexion.php'>Se connecter</a>";
+		} else {
+			if($_SESSION["statut"] == 1){
+				echo "<a href='affichage_admin.php'>Affichage boutique (admin)</a><br>";
+				echo "<a href='ajout_boutique.php'>Ajout boutique (admin)</a><br>";
+			}
+			echo "<a href='creator.php'>Creer son t-shit personalisé</a><br>";
+			echo "<a href='boutique_client.php'>boutique</a><br>";
+		    echo "<a href='logout.php'>Se deconnecter</a><br>";
+		}
+		?>
+	</nav>
 </body>
 </html>
