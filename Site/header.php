092717582d4af128a1664070c7d2ac1e0ff09874
Woople74 <Darkmar74@outlook.com>
2020-05-18 14:04:27 +0200

    je continue le header mais c'est grave moche pour le moment regardez pas mdr

--- Site/header.php
+++ Site/header.php
@@ -2,12 +2,12 @@
 	<header>
 	    <div class='logo'>
 	        <a href='index.php'><img src='data/img/iconne_site.png' alt='iconne_site' height='100' class= 'logo_img'></a>
-	    </div><br>
+	    </div>
 	    <nav>
 	        <ul>
 	        <?php
 	        if(!isset($_SESSION["pseudo"])) {
-	            echo "<h3>Vous n'êtes pas connecté(e)</h3><br>";
+	            echo "Vous n'êtes pas connecté(e)<br>";
 	            echo "<li><a href='inscription.php'>S'inscrire</a></li>";
 	            echo "<li><a href='connexion.php'>Se connecter</a></li>";
 	        } else {
@@ -15,7 +15,7 @@
 	                echo "<li><a href='affichage_admin.php'>Affichage boutique (admin)</a></li>";
 	                echo "<li><a href='ajout_boutique.php'>Ajout boutique (admin)</a></li>";
 	            }
-	            echo "<li><a href='creator.php'>Creer son t-shit personalisé</a></li>";
+	            echo "<li><a href='creator.php'>Créer son T-shirt personnalisé</a></li>";
 	            echo "<li><a href='boutique_client.php'>Boutique</a></li>";
 	            echo "<li><a href='logout.php'>Se deconnecter</a></li>";
 	        }
