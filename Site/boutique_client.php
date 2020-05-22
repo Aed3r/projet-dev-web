69630941fed7eadd01f33647d4f567d9d187247d
Woople74 <Darkmar74@outlook.com>
2020-05-22 21:41:32 +0200

    Système de recherche dans la boutique (par rapport a la description)

--- Site/boutique_client.php
+++ Site/boutique_client.php
@@ -2,6 +2,11 @@
 session_start();
 include 'bdd/connex.inc.php';
 $pdo = connex();
+if(isset($_POST["submit"])) {
+	$recup = $pdo->query('SELECT * FROM Produits WHERE description LIKE "%' . $_POST["recherche"] .'%"');
+}else{
+	$recup = $pdo->query('SELECT * FROM Produits');
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,26 +19,27 @@ $pdo = connex();
 <body>
 	<?php
 	include 'header.php';
-	$recup= $pdo->query('SELECT * FROM Produits');
+	?>
+	<form class='search' action='boutique_client.php' method='post' enctype='multipart/form-data'>
+        <label for="recherche">
+        	<input type="text" name="recherche" id="recherche">
+        </label>
+        <input type='submit' value='search' name='submit'>
+    </form> 
+	<?php
+	/*On fait un premier fetch pour voir si notre requête est vide*/
+	$donnees = $recup->fetch();
+	if(empty($donnees) && isset($_POST["submit"])){
+		/*Si elle l'est on affiche un message d'erreur*/
+		echo "Désolé mais aucun résultat n'a été trouvé pour la recherche :" . $_POST["recherche"];
+	}else{
+		/*Sinon on affiche le premier produit avant de lancer la boucle (Sinon on loupe le premier produit)*/
+		echo '<a href="precision_produit.php?var= '.$donnees['id'].'" class="t-shirt"><img src="' . $donnees['lien_image'] . '" width="300" height="250" ></a>'.$donnees['prix'].'€';
+	}
 	while ($donnees = $recup->fetch())
 	{
-	    echo '<a href="precision_produit.php?var= '.$donnees['id'].'" class="t-shirt"><img src="' . $donnees['lien_image'] . '" width="300" height="250" ></a>';
+	    echo '<a href="precision_produit.php?var= '.$donnees['id'].'" class="t-shirt"><img src="' . $donnees['lien_image'] . '" width="300" height="250" ></a>'.$donnees['prix'].'€';
 	}
-
-
-	//echo '<img src="' . $lien . '" width="300" height="250" >';
-	//echo  '<img src = "Images/iron_maiden.jpg"/>';
-
-	//echo '<a href="1st_tshirt.php"> <img src="Images/iron_maiden.jpg" width="300" height="250" /> </a>';
-	//echo '<img src="Images/metallica_basique.jpg" width="300" height="250" /></div> ';
-	//echo '<img src="Images/t-shirt-chirac-thug-life_noir.jpg" width="250" height="300" /></div> ';
-
-	function test() {
-	    echo 'bonjour';
-	}
-
-	//echo '<a href="precision_produit.php?var=pierre">Lien pierre</a>';
-
 	?>
 </body>
 </html>
