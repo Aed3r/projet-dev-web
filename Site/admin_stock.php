75572a6cb3aadca1c4e0a03af0c8c9a1a0c57b09
Woople74 <Darkmar74@outlook.com>
2020-05-28 23:37:02 +0200

    visionnage du stock pour les admins

---
+++ Site/admin_stock.php
@@ -0,0 +1,35 @@
+<!-- Cette page sers a visualiser sur un tableau les différentes tailles et en quelles quantités elles sont disponibles -->
+<!-- On suppose que le stock est modifié automatiquement par les employés qui s'occupent des marchandises -->
+<?php 
+session_start();
+if(!isset($_SESSION["pseudo"])) {
+    header('Location:index.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Stock disponible</title>
+</head>
+<body>
+	<table>
+		<tr>
+			<th>Taille</th>
+			<th>Quantité</th>
+		</tr>
+	<?php
+		include 'bdd/connex.inc.php';
+		$pdo = connex();
+		$reponse= $pdo->query('SELECT * FROM disponibilité WHERE id = '. $_GET['val']);
+		echo '<h1> Stock disponible pour ce t-shirt </h1>';
+		while($donnee = $reponse->fetch()){
+			echo '<tr>';
+			echo '<td>'.$donnee['taille'].'</td>';
+			echo '<td>'.$donnee['quantité'].'</td>';
+			echo '</tr>';
+		}
+		$pdo = null;
+	?>
+	</table>
+</body>
+</html>
