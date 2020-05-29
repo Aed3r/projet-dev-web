7565f15f5298d2baaefbbd8109b1ea671b780d14
Aed3r <xxgoffi22xx@gmail.com>
2020-05-29 17:38:22 +0200

    mise en page

--- Site/index.php
+++ Site/index.php
@@ -7,14 +7,37 @@ session_start();
 	<title>MonTshirt.fr</title>
 	<meta charset="utf-8">
 	<link rel="icon" href="data/img/iconne_site.png">
-    <link rel="stylesheet" href="data/general_style.css"> 
+	<link rel="stylesheet" href="data/general_style.css"> 
+	<link rel="stylesheet" href="data/indexStyle.css"> 
 </head>
 <body>
 	<!-- On include le fichier qui contient le header car il est identique sur toutes les pages -->
 	<?php include 'header.php'?>
-	<article>
-		Ce site internet vous premetra de choisir parmis une grande variété de t-shirts des plus grandes marques.<br>
-		Si votre soif de personnalisation est plus grande que notre catalogue un createur interractif de T-shirt est également disponible.
-	</article>
+
+	<div id="main">
+		<div id="boutique">
+			<img src="data/img/boutique.png">
+			<div>
+				<article>
+				Ce site internet vous premetra de choisir parmis une grande variété de t-shirts répondant à tous vos désirs.<br>
+				</article>
+				<br>
+				<a href="boutique_client.php"> <input type="button" value="Vers la boutique"> </a>
+			</div>
+		</div>
+		<br>
+		<div id="createur">
+			<div>
+				<article>
+				Si votre soif de personnalisation est plus grande que notre catalogue,<br> 
+				un créateur interactif de T-shirt est également disponible.<br> 
+				Nous vous garantissons que personne ne jugera vos goûts artistique.
+				</article>
+				<br>
+				<a href="creator.php"> <input type="button" value="Vers le créateur"> </a>
+			</div>
+			<img src="data/img/createur.png">
+		</div>
+	</div>
 </body>
 </html>
